6407924cdde7b40d61d32afa9e40c0070e453ae9
Mohammed Nuwayfi AL-Omari <72771492+Hamatchi@users.noreply.github.com>
2022-04-30 00:11:44 +0300

    Add files via upload

---
+++ Cipher Hema/Cipher Graphy/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Cipher_Graphy
+{
+    partial class Cipher
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.progressBar2);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(971, 573);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(888, 411);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 30);
+            this.label6.TabIndex = 14;
+            this.label6.Text = " ";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(59, 411);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(825, 40);
+            this.progressBar2.TabIndex = 13;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(59, 411);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(825, 40);
+            this.progressBar1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(260, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 30);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "......";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(803, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Encryption";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(154, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 30);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Status:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(121, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 30);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Key Secret:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(149, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 30);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Folder:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(175, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 30);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "File:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(803, 95);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 40);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Decryption";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(425, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Select Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(241, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add Folder";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(241, 136);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(329, 35);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(241, 95);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(543, 35);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(241, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(543, 35);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Cipher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 567);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Cipher";
+            this.Text = "1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Cipher Hema/Cipher Graphy/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.Security.Cryptography;
+
+
+
+
+
+namespace Cipher_Graphy
+
+
+//=======================================//
+//              AES 256 Bit              //
+//=======================================//
+//   "  QnkgTWFkZSBIYW1hdGNoaQ==   "     //
+
+
+
+{
+    public partial class Cipher : Form
+    {
+
+        BackgroundWorker background1 = new BackgroundWorker();
+        BackgroundWorker background2 = new BackgroundWorker();
+
+        public Cipher()
+        {
+
+            InitializeComponent();
+
+            background1.WorkerReportsProgress = true;
+            background1.WorkerSupportsCancellation = true;
+
+            background1.DoWork += Background1_DoWork;
+            background1.ProgressChanged += Background1_ProgressChanged;
+            background1.RunWorkerCompleted += Background1_RunWorkerCompleted;
+
+
+            background2.WorkerReportsProgress = true;
+            background2.WorkerSupportsCancellation = true;
+
+            background2.DoWork += Background2_DoWork;
+            background2.ProgressChanged += Background2_ProgressChanged;
+            background2.RunWorkerCompleted += Background2_RunWorkerCompleted;
+        }
+
+        private void Background2_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("The File Has Been Successfully Decrypted");
+            progressBar2.Value = 0;
+            label6.Text = "0%";
+
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+        }
+
+        private void Background2_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            progressBar2.Value = e.ProgressPercentage;
+            label6.Text = progressBar2.Value + "%";
+            label5.Text = "Decrypted File";
+        }
+
+        private void Background2_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            getDecryptionFille(pass, salt, fileName, finallFolderTaget);
+        }
+
+        private void Background1_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("The File Has Been Successfully Encrypted");
+            progressBar1.Value = 0;
+            label6.Text = "0%";
+            label5.Text = ". . . . . . . . . ";
+            button3.Enabled = true;
+            button4.Enabled = true;
+        }
+
+        private void Background1_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label6.Text = progressBar1.Value + "%";
+            label5.Text = "Encryptet File";
+
+        }
+
+        // ==================================================================
+        //                             Vars
+        // ================================================================== 
+
+        string fileName = string.Empty;
+        string folderTarget = string.Empty;
+        string finallFolderTaget = string.Empty;
+        string pass = string.Empty;
+        string salt = "choice any key";
+        
+
+
+
+
+
+        // ==================================================================
+        //                          Backgrounds
+        // ==================================================================
+
+        private void Background1_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            getEncryptionFile(pass, salt, fileName, finallFolderTaget);
+        }
+
+        // ==================================================================
+        //                          The Form
+        // ==================================================================
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pass = textBox3.Text;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            progressBar1.Hide();
+            progressBar2.Show();
+            background2.RunWorkerAsync();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog.FileName;
+                textBox1.Text = openFileDialog.FileName;
+
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                folderTarget = folderBrowserDialog.SelectedPath;
+                //textBox2.Text = folderBrowserDialog.SelectedPath;
+
+            }
+
+            folderTarget += @"\";
+            folderTarget += Path.GetFileName(folderTarget);
+            
+            for (int i = 0; i < folderTarget.Length; i++)
+            {
+                if (i == 3 && folderTarget[i] == '\\')
+                {
+                    finallFolderTaget = folderBrowserDialog.SelectedPath;
+                    finallFolderTaget += Path.GetFileName(fileName);
+                }
+                {
+                    if (i == 3 && folderTarget[i] != '\\') 
+                    {
+                        finallFolderTaget = folderBrowserDialog.SelectedPath;
+                        finallFolderTaget += @"\";
+                        finallFolderTaget += Path.GetFileName(fileName);
+                    }
+                }
+            }
+
+            textBox2.Text = finallFolderTaget;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            button4.Enabled = false;
+            pass = textBox3.Text;
+            progressBar1.Show();
+            progressBar2.Hide();
+           background1.RunWorkerAsync();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // ==================================================================
+        //                          The Actions
+        // ==================================================================
+
+
+        private void getEncryptionFile(string strKey, string strByte, string inputFile, string outFolderTarget)
+        {
+            byte[] keyByte = Encoding.UTF8.GetBytes(strKey);
+            byte[] saltByte = Encoding.UTF8.GetBytes(strByte);
+            try
+            {
+                using(var aes = new RijndaelManaged())
+                {
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.Mode = CipherMode.CBC;
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(keyByte, saltByte, 50000);
+                    aes.Key = key.GetBytes(aes.KeySize / 8);
+                    aes.IV = key.GetBytes(aes.BlockSize / 8);
+
+
+                    using (var stream1 = new FileStream(outFolderTarget, FileMode.Create))
+                    {
+                        stream1.Write(saltByte, 0, saltByte.Length);
+                        using (var ICryptoStream = new CryptoStream(stream1, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            using (var Stream2 = new FileStream(inputFile, FileMode.Open))
+                            {
+                                byte[] buffer = new byte[1048576];
+                                int readAllBytes;
+                                while ((readAllBytes = Stream2.Read(buffer, 0, buffer.Length)) > 0)
+                                {
+                                    ICryptoStream.Write(buffer, 0, buffer.Length);
+                                    background1.ReportProgress((int)(stream1.Position * 100 / Stream2.Length));
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void getDecryptionFille(string strKey, string strByte, string inputFile, string outFolderTarget)
+        {
+            byte[] keyByte = Encoding.UTF8.GetBytes(strKey);
+            byte[] saltByte = Encoding.UTF8.GetBytes(strByte);
+            try
+            {
+              
+                using(var aes = new RijndaelManaged())
+                {
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.Mode = CipherMode.CBC;
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(keyByte, saltByte, 50000);
+                    aes.Key = key.GetBytes(aes.KeySize / 8);
+                    aes.IV = key.GetBytes(aes.BlockSize / 8);
+
+                    using (var stream1 = new FileStream(inputFile, FileMode.Open))
+                    {
+                        stream1.Read(saltByte, 0, saltByte.Length);
+                        using(var iCryptoStream = new CryptoStream(stream1, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                        {
+                            using(var stream2 = new FileStream(outFolderTarget, FileMode.Create))
+                            {
+                                byte[] buffer = new byte[1048576];
+                                int readAllBytes;
+                                while ((readAllBytes = iCryptoStream.Read(buffer, 0, buffer.Length)) > 0 )
+                                {
+                                    stream2.Write(buffer, 0, buffer.Length);
+                                    background2.ReportProgress((int)(stream1.Position * 100 / stream1.Length));
+                                }
+                            }    
+                        }    
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+    }
+}
